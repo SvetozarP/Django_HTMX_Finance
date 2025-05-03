e59e22c998aa00ac08e18a2029755bfbbff8e1ba
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-03 21:25:57 +0100

    Updated requirements. Added date filter powered by htmx.

--- tracker/filters.py
+++ tracker/filters.py
@@ -1,4 +1,6 @@
 import django_filters
+from django import forms
+
 from tracker.models import Transaction
 
 class TransactionFilter(django_filters.FilterSet):
@@ -10,6 +12,20 @@ class TransactionFilter(django_filters.FilterSet):
         empty_label='Any',
     )
 
+    start_date = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='gte',
+        label='Date From',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    )
+
+    end_date = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='lte',
+        label='Date To',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    )
+
     class Meta:
         model = Transaction
-        fields = ['transaction_type']
+        fields = ['transaction_type', 'start_date', 'end_date']
